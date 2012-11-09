2f9f4cf7cf2e775663294c922f036a4a59402a2d
sergiokas <sergiokas@gmail.com>
2012-11-09 13:26:40 -0300

    adding required files

---
+++ libraries/drivers/Cache/Mongo.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+/**
+* MongoDB based Cache driver.
+*
+* $Id: Cache_Mongo_Driver.php 0 2012-11-09 00:00:00Z sergiokas $
+*
+* @package    Cache
+* @author     sergiokas <dev@sergiokas.com>
+* @require    php-mongo
+* @license    http://kohanaphp.com/license.html
+*/
+class Cache_Mongo_Driver implements Cache_Driver {
+
+
+
+}
